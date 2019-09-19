872e11d6bb17bbada3b3db9e3e113e4639f93102
JasonZhanggg <383349@bsd48.org>
2019-09-19 10:17:05 -0700

    Palindrome

---
+++ Palindrome/Palindrome.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main(){
+  //char array for input
+  char input[80];
+  char newInput[80];
+  //take in char array and include spaces
+  cin.getline(input, sizeof(input));
+  //loop through the input array
+  for(int i = 0, k = 0; i<sizeof(input); i++){
+    //if it is not punctuation
+    if(!(ispunct(input[i]) || input[i] == ' ' || input[i] == '"')){
+      //add it to a new array
+      newInput[k] = tolower(input[i]);
+      k++;
+    }
+  }
+  //loop through the the first and second half of the array
+  for(int i = 0, k = strlen(newInput)-1; i < k ; i++, k--){
+    //if the first digit and the last digit equal, check if the second digit and the second last digit are equal, etc
+    if(newInput[i] != newInput[k]){
+      //if they are not equal, print not a palindrome and return
+      cout << "Not Palindrome";
+      return 0;
+    }
+    //if they are a palindrome print Palindrome and return
+    cout << "Palindrome";
+    return 0;
+  }
+}
